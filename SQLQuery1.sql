77355c2c00d00f457146ec8601c8934e7e12c20e
Tokyo <moudi0u0@gmail.com>
2024-10-28 06:09:02 +0200

    Inner join_

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -879,34 +879,34 @@
 --------------------------------------------------------------------------------------------------------------------
 
 
-USE Shop_Database;
+--USE Shop_Database;
 
---Inner Join
-SELECT Customers.CustomerID, Customers.Name, Orders.Amount
-FROM Customers
-INNER JOIN Orders
-ON Customers.CustomerID = Orders.CustomerID
-
-
---Left Join
-SELECT Customers.CustomerID, Customers.Name, Orders.Amount
-FROM Customers
-LEFT OUTER JOIN Orders
-ON Customers.CustomerID = Orders.CustomerID
+----Inner Join
+--SELECT Customers.CustomerID, Customers.Name, Orders.Amount
+--FROM Customers
+--INNER JOIN Orders
+--ON Customers.CustomerID = Orders.CustomerID
 
 
---Right Join
-SELECT Customers.CustomerID, Customers.Name, Orders.Amount
-FROM Customers 
-RIGHT OUTER JOIN Orders
-ON Customers.CustomerID = Orders.CustomerID
+----Left Join
+--SELECT Customers.CustomerID, Customers.Name, Orders.Amount
+--FROM Customers
+--LEFT OUTER JOIN Orders
+--ON Customers.CustomerID = Orders.CustomerID
 
 
---Full Join
-SELECT Customers.CustomerID, Customers.Name, Orders.Amount
-FROM Customers 
-FULL OUTER JOIN Orders
-ON Customers.CustomerID = Orders.CustomerID
+----Right Join
+--SELECT Customers.CustomerID, Customers.Name, Orders.Amount
+--FROM Customers 
+--RIGHT OUTER JOIN Orders
+--ON Customers.CustomerID = Orders.CustomerID
+
+
+----Full Join
+--SELECT Customers.CustomerID, Customers.Name, Orders.Amount
+--FROM Customers 
+--FULL OUTER JOIN Orders
+--ON Customers.CustomerID = Orders.CustomerID
 
 
 --------------------------------------------------------------------------------------------------------------------
